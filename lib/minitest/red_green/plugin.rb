6eab65f6da556a2c5b1d22f0a096279fc9941583
ntl <nathanladd+github@gmail.com>
2014-09-06 15:42:53 -0500

    Big time git fail

---
+++ lib/minitest/red_green/plugin.rb
@@ -0,0 +1,49 @@
+module Minitest
+  module RedGreen
+    class Plugin
+      attr_accessor :sync
+
+      ESC = "\e["
+
+      def initialize io
+        @io = io
+      end
+
+      def puts *strings
+        if not strings.grep(%r{\d+\) (?:Failure|Error)}).empty?
+          strings.map! &method(:red)
+        end
+        @io.puts *strings
+      end
+
+      def print str
+        @io.print filter_test_output_chars str
+      end
+
+      def filter_test_output_chars str
+        case str
+        when '.' then green str
+        when 'E', 'F' then red str
+        when 'S' then yellow str
+        else str
+        end
+      end
+
+      def green str
+        colorize str, 32
+      end
+
+      def red str
+        colorize str, 31
+      end
+
+      def yellow str
+        colorize str, 33
+      end
+
+      def colorize str, col_code
+        "#{ESC}#{col_code}m#{str}#{ESC}0m"
+      end
+    end
+  end
+end
--- lib/minitest/red_green/version.rb
+++ lib/minitest/red_green/version.rb
@@ -1,5 +1,5 @@
 module Minitest
   module RedGreen
-    VERSION = "0.9.1"
+    VERSION = "0.9.2"
   end
 end
